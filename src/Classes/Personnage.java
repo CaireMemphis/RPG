74676489743b824ca8cbdf46c2218be031937897
Thomas Screve <thomas-scrve@laposte.net>
2022-05-17 16:11:40 +0200

    exo rpg

---
+++ src/Classes/Personnage.java
@@ -0,0 +1,59 @@
+package Classes;
+
+import java.util.Objects;
+
+public class Personnage {
+    private int pv;
+    private int atk;
+    private String nom;
+
+    public Personnage(String nom) {
+        this.nom = nom;
+    }
+
+    public int getPv() {
+        return pv;
+    }
+
+    public void setPv(int pv) {
+        this.pv = pv;
+    }
+
+    public int getAtk() {
+        return atk;
+    }
+
+    public void setAtk(int atk) {
+        this.atk = atk;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    @Override
+    public String toString() {
+        return "Personnage{" +
+                "pv=" + pv +
+                ", atk=" + atk +
+                ", nom='" + nom + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Personnage that = (Personnage) o;
+        return pv == that.pv && atk == that.atk && Objects.equals(nom, that.nom);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(pv, atk, nom);
+    }
+}
---
+++ src/Interfaces/IPersonnage.java
@@ -0,0 +1,7 @@
+package Interfaces;
+
+import Classes.Personnage;
+
+public interface IPersonnage {
+    public Personnage personnageFactory();
+}
